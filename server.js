797e8232edc26594c1829bbe1ee17e3f85afb30c
DukeHdez <briayan27@outlook.com>
2022-05-25 13:18:25 -0400

    Build database calls

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+const config = require('config');
+const user = config.get('server.user');
+const pw = config.get('server.password');
+const database = config.get('server.database');
+
+//MySql
+const mysql = require('mysql2');
+//Express
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +14,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: user,
+        password: pw,
+        database: database
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
